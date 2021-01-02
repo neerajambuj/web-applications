e81d63c82d081ae60a28e848ddeb4e134539bbb0
neerajk <neerajk@india.tejasnetworks.com>
2021-01-02 14:22:28 +0530

    Bug Fixed

--- Procfile
+++ Procfile
@@ -1,3 +1,1 @@
 web: gunicorn discord_bot.wsgi
-web:    lein run -m discord_bot.web
-worker: lein run -m discord_bot.worker
--- discord_bot/settings.py
+++ discord_bot/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
-#import django_heroku
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
@@ -126,4 +126,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 STATIC_URL = '/static/'
 #set DISABLE_COLLECTSTATIC=1
-#django_heroku.settings(locals())
+django_heroku.settings(locals())
